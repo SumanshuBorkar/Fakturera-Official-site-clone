d9ed047cc7b59577debd004b0135f3e88fa930dd
Sumanshu <101553319+SumanshuBorkar@users.noreply.github.com>
2025-08-22 00:55:46 +0530

    fixed the white screen effect

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='appcont'>
+      <App />
+    </div>
   </StrictMode>,
 )
